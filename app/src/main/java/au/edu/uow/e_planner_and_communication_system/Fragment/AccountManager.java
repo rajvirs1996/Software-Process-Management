7b8751edec0fbe3e2b8612d7c5b3460104cabac9
manishmall20 <manish147220@gmail.com>
2018-03-08 10:34:42 +0800

    Updated Account Manager & Sign Up to store users data on firebase

--- app/src/main/java/au/edu/uow/e_planner_and_communication_system/Fragment/AccountManager.java
+++ app/src/main/java/au/edu/uow/e_planner_and_communication_system/Fragment/AccountManager.java
@@ -3,10 +3,13 @@ package au.edu.uow.e_planner_and_communication_system.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.*;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import au.edu.uow.e_planner_and_communication_system.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Athens on 2018/02/27.
@@ -14,10 +17,18 @@ import au.edu.uow.e_planner_and_communication_system.R;
 
 public class AccountManager extends Fragment {
 
+    private CircleImageView accountManagerDisplayImagae;
+    private TextView accountManagerDisplayName;
+    private TextView getAccountManagerDisplaystatus;
+    private Button accountManagerChangeName;
+    private Button accountManagerChangePassword;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        accountManagerDisplayImagae = (CircleImageView)
 
         return inflater.inflate(R.layout.accountmanager, container, false);
     }

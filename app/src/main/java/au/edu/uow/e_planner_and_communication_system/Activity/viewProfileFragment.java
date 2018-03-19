70c0d9c80848b8d694d9c945a6a29a4a504b3d69
manishmall20 <manish147220@gmail.com>
2018-03-19 21:00:11 +0800

    Minor Updates to viewProfile

--- app/src/main/java/au/edu/uow/e_planner_and_communication_system/Activity/viewProfileFragment.java
+++ app/src/main/java/au/edu/uow/e_planner_and_communication_system/Activity/viewProfileFragment.java
@@ -1,7 +1,13 @@
-package au.edu.uow.e_planner_and_communication_system.Fragment;
+package au.edu.uow.e_planner_and_communication_system.Activity;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,7 +22,7 @@ import com.squareup.picasso.Picasso;
 import au.edu.uow.e_planner_and_communication_system.R;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class viewProfileFragment extends AppCompatActivity {
+public class viewProfileFragment extends DialogFragment {
 
     private Button messageButton;
     private Button addButton;
@@ -25,27 +31,41 @@ public class viewProfileFragment extends AppCompatActivity {
     private TextView studentID;
     private TextView email;
     private CircleImageView profilePic;
+    private String ID;
 
     private DatabaseReference usersReference;
 
+
+    public void setID(String ID){
+
+        this.ID = ID;
+
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_profile);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_view_profile,null);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         usersReference = FirebaseDatabase.getInstance().getReference().child("Users");
 
 
 
-        String visit_profile_id = getIntent().getExtras().get("visit_profile_id").toString();
+        String visit_profile_id = ID;
 
-        messageButton = (Button) findViewById(R.id.viewprofile_message);
-        addButton = (Button) findViewById(R.id.viewprofile_add);
-        removeButton = (Button) findViewById(R.id.viewprofile_remove);
-        name = (TextView) findViewById(R.id.viewprofile_display_name);
-        studentID = (TextView) findViewById(R.id.viewprofile_sid_text_view);
-        email = (TextView) findViewById(R.id.viewprofile_email_text);
-        profilePic = (CircleImageView) findViewById(R.id.viewprofile_image);
+        messageButton = (Button) view.findViewById(R.id.viewprofile_message);
+        addButton = (Button) view.findViewById(R.id.viewprofile_add);
+        removeButton = (Button) view.findViewById(R.id.viewprofile_remove);
+        name = (TextView) view.findViewById(R.id.viewprofile_display_name);
+        studentID = (TextView) view.findViewById(R.id.viewprofile_sid_text_view);
+        email = (TextView) view.findViewById(R.id.viewprofile_email_text);
+        profilePic = (CircleImageView) view.findViewById(R.id.viewprofile_image);
 
         usersReference.child(visit_profile_id).addValueEventListener(new ValueEventListener() {
 
@@ -69,6 +89,5 @@ public class viewProfileFragment extends AppCompatActivity {
 
             }
         });
-
     }
 }
--- app/src/main/java/au/edu/uow/e_planner_and_communication_system/Fragment/ContactsFragment.java
+++ app/src/main/java/au/edu/uow/e_planner_and_communication_system/Fragment/ContactsFragment.java
@@ -1,16 +1,17 @@
 package au.edu.uow.e_planner_and_communication_system.Fragment;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -23,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
+import au.edu.uow.e_planner_and_communication_system.Activity.viewProfileFragment;
 import au.edu.uow.e_planner_and_communication_system.R;
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -48,6 +50,18 @@ public class ContactsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.contacts, container, false);
+    }
+
+    public void showDialog(View view, String ID)
+    {
+        String idToPass = ID;
+
+        FragmentManager manager = getFragmentManager();
+        viewProfileFragment viewProfileDialog = new viewProfileFragment();
+        viewProfileDialog.setID(idToPass);
+        viewProfileDialog.show(manager,"MyDialog");
+
+
     }
 
     public void onViewCreated (View view, Bundle savedInstanceState){
@@ -81,9 +95,13 @@ public class ContactsFragment extends Fragment {
                                 //Gets the unique when selected
                                 String vist_profile_id = getRef(position).getKey();
 
-                                Intent profileIntent = new Intent(getActivity(), viewProfileFragment.class);
+                                /*Intent profileIntent = new Intent(getActivity(), viewProfileFragment.class);
                                 profileIntent.putExtra("visit_profile_id",vist_profile_id);
-                                startActivity(profileIntent);
+                                startActivity(profileIntent);*/
+
+                                showDialog(view,vist_profile_id);
+
+
                             }
                         });
                     }
@@ -111,6 +129,7 @@ public class ContactsFragment extends Fragment {
         super.onStop();
         firebaseRecyclerAdapter.stopListening();
     }
+
 
     public static class allUsersViewHolder extends RecyclerView.ViewHolder
     {

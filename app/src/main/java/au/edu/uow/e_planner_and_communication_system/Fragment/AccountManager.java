cbc568613c38487827e24c20cd42e12dfd4c5fd6
Tony <at5393-5394@yahoo.com.hk>
2018-03-28 16:16:22 +0800

    DO NOT REVERT: GFX Update & PW Change option

--- app/src/main/java/au/edu/uow/e_planner_and_communication_system/Fragment/AccountManager.java
+++ app/src/main/java/au/edu/uow/e_planner_and_communication_system/Fragment/AccountManager.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -188,6 +189,8 @@ public class AccountManager extends Fragment {
        user_statusRef.addValueEventListener(new ValueEventListener() {
            @Override
            public void onDataChange(DataSnapshot dataSnapshot) {
+
+               final String status = (String) dataSnapshot.getValue();
 
                 if (status.equals("Offline")) {
 
